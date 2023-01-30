0fd18c383080f93732248ccb242152415cf5df0e
nlorioux <nolwenn.lorioux@eleves.ec-nantes.fr>
2023-01-30 17:21:38 +0100

    get what on PC

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
+    public GameObject Camera;
     public float walkSpeed = 10.0f;
     public float rotationSpeed = 180.0f;
+
+    private void Start()
+    {
+        //Camera = GameObject.FindGameObjectWithTag("MainCamera");
+        //Camera.transform.parent = transform;
+    }
 
     // Update is called once per frame
     void Update()
     {
+        /*
         //Controlling character's movement
         if (Input.GetKey(KeyCode.UpArrow))
         {
@@ -19,7 +27,16 @@ public class Character : MonoBehaviour
         {
             transform.Translate(Vector3.back * Time.deltaTime * walkSpeed);
         }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * walkSpeed);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * walkSpeed);
+        }
         //Controlling charater's rotation
-        transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0) * Time.deltaTime * rotationSpeed);
+        //Camera.transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0) * Time.deltaTime * rotationSpeed);
+        */
     }
 }

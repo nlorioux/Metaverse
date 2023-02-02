017c293dbaef8ce51f0555a5006cc92b33e79ec1
OnsChahloul <hamzamajidtmnttmnt@gmail.com>
2023-02-02 14:38:03 +0100

    lil push

--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -4,6 +4,62 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
+    [SerializeField] GameObject cameraHolder;
+    [SerializeField] float mousSensitivity, sprintSpeed, walkSpeed, jumpForce, smoothTime;
+
+    float verticalLookRotation;
+    bool grounded;
+    Vector3 smoothMoveVelocity;
+    Vector3 moveAmount;
     Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Look();
+        Move();
+        Jump();
+       
+
+       
+
+    }
+
+    void Move()
+    {
+        Vector3 movDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        moveAmount = Vector3.SmoothDamp(moveAmount, movDir * (Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : walkSpeed), ref smoothMoveVelocity, smoothTime);
+    } 
+
+    void Jump ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            rb.AddForce(transform.up * jumpForce);
+        }
+    }
+
+    void Look ()
+    {
+        transform.Rotate(Vector3.up * Input.GetAxisRaw("Mouse X") * mousSensitivity);
+        verticalLookRotation += Input.GetAxisRaw("Mouse Y") * mousSensitivity;
+        verticalLookRotation = Mathf.Clamp(verticalLookRotation, -90f, 90f);
+
+        cameraHolder.transform.localEulerAngles = Vector3.left * verticalLookRotation;
+    }
+
+    public void SetGroundedState (bool _grounded)
+    {
+        grounded = _grounded;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + transform.TransformDirection(moveAmount) * Time.fixedDeltaTime);
+    }
 
 }

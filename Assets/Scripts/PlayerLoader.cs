51e930c8cd03e0c544fb374d176359e5b7998816
nlorioux <nolwenn.lorioux@eleves.ec-nantes.fr>
2023-02-06 17:09:59 +0100

    Load avatar in position in PUN scene

---
+++ Assets/Scripts/PlayerLoader.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ReadyPlayerMe;
+
+public class PlayerLoader : MonoBehaviour
+{
+    [SerializeField]
+    private string avatarURL = "https://models.readyplayer.me/63dcc1189b552e12bccd75e8.glb";
+
+    private GameObject avatar;
+    //private GameObject avatarPUNPrefab;
+
+    private void Start()
+    {
+        //LoadAvatar();
+
+        //ApplicationData.Log();
+        //avatarPUNPrefab = GameObject.FindGameObjectWithTag("Controller");
+        var avatarLoader = new AvatarLoader();
+        avatarLoader.OnCompleted += (_, args) =>
+        {
+            avatar = args.Avatar;
+
+            avatar.transform.parent = gameObject.transform;
+            avatar.transform.position = avatar.transform.parent.position - new Vector3(0, 1f, 0);
+
+            AvatarAnimatorHelper.SetupAnimator(args.Metadata.BodyType, avatar);
+        };
+        avatarLoader.LoadAvatar(avatarURL);
+    }
+
+
+    private void OnDestroy()
+    {
+        if (avatar != null) Destroy(avatar);
+    }
+
+    /*private void LoadAvatar()
+    {
+        AvatarLoader avatarLoader = new AvatarLoader();
+        avatarLoader.LoadAvatar(avatarURL);
+    }
+
+    private void AvatarLoadComplete (object sender, CompletionEventArgs args)
+    {
+        var context = new AvatarContext();
+        var avatar = (GameObject)context.Data;
+        avatar.transform.parent = avatarPUNPrefab.transform;
+        Debug.Log($"Avatar loaded");
+    }
+
+    private void AvatarLoadFail(object sender, FailureEventArgs args)
+    {
+        Debug.Log($"Avatar loading failed with error message: {args.Message}");
+    }
+    */
+}
